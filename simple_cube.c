00f5de69b8853a0ff744fbfa62b4f8169ba4f108
chivada3301 <chaimabekhouche2025@gmail.com>
2025-12-31 14:38:55 +0100

    Create simple_cube.c

---
+++ simple_cube.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a, b ,c ,d ,e ,f;
+
+    printf("please enter the first number a \n");
+    scanf("%d",&a);
+
+    d=a*a*a;
+    printf("the cube of a is=%d \n",d);
+
+    printf("please enter the first number b \n");
+    scanf("%d",&b);
+
+    e=b*b*b;
+    printf("the cube of b is=%d \n",e);
+
+    printf("please enter the first number c \n");
+    scanf("%d",&c);
+
+    f=c*c*c;
+    printf("the cube of c is=%d \n",f);
+
+    return 0;
+}
